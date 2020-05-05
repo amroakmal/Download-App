9059e13e9dd3d8da36ecb52862c68376361c5563
amroakmal <amrakmal97@gmail.com>
2020-05-05 18:05:57 +0200

    Make needed methods signature

--- index.py
+++ index.py
@@ -8,18 +8,30 @@ from os import path
 FORM_CLASS, _ = loadUiType(path.join(path.dirname(__file__), "download.ui"))
 
 
-class mainapp(QMainWindow, FORM_CLASS):
+class MainApp(QMainWindow, FORM_CLASS):
     def __init__(self, parent=None):
-        super(mainapp, self).__init__(parent)
+        super(MainApp, self).__init__(parent)
         QMainWindow.__init__(self)
         self.setupUi(self)
+
+    def Handle_Buttons(self):
+        pass
+
+    def HandleBrowse(self):
+        pass
+
+    def HandleProgressBar(self):
+        pass
+
+    def HandleStartDownload(self):
+        pass
 
 
 def main():
     app = QApplication(sys.argv)
-    window = mainapp()
+    window = MainApp()
     window.show()
-    app.exec()
+    app.exec_()
 
 
 if __name__ == '__main__':
