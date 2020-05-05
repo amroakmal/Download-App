2dc611a066ff29dae3fb280a92e5656b760ed154
amroakmal <amrakmal97@gmail.com>
2020-05-05 16:00:45 +0200

    Connect to Qt5

--- index.py
+++ index.py
@@ -1,8 +1,27 @@
-from PyQt5 import *
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.uic import loadUi, loadUiType
+import sys
+
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.uic import loadUiType
 
 from os import path
 
 FORM_CLASS, _ = loadUiType(path.join(path.dirname(__file__), "download.ui"))
+
+
+class mainapp(QMainWindow, FORM_CLASS):
+    def __init__(self, parent=None):
+        super(mainapp, self).__init__(parent)
+        QMainWindow.__init__(self)
+        self.setupUi(self)
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = mainapp()
+    window.show()
+    app.exec()
+
+
+if __name__ == '__main__':
+    main()
+
